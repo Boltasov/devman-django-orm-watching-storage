ff9fad8fc81e81eeee09d16ff21e5862b89bd515
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-05-17 18:58:17 +0300

    move sensitive data to .env

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,16 @@
 import os
+from environs import Env
 
+env = Env()
+env.read_env()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': env('ENGINE'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
     }
 }
 
@@ -15,7 +18,7 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = 'REPLACE_ME'
 
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
