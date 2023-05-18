dcb2fb6a28e2597e200f2c9c8683af8eb3e88876
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-05-18 15:41:09 +0300

    hide sensitive data to env variables

--- project/settings.py
+++ project/settings.py
@@ -16,13 +16,13 @@ DATABASES = {
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', True)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
